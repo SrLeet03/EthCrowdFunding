721e453f7b0b4875479dcb70bf60980d947849f9
vishwasp3000 <vishwas_p@ch.iitr.ac.in>
2023-02-14 15:21:30 +0530

    push abi

--- solidity-hardhat/hardhat.config.js
+++ solidity-hardhat/hardhat.config.js
@@ -6,6 +6,13 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+
+const PRIVATE_KEY1 = process.env.PRIVATE_KEY1
+const PRIVATE_KEY2 = process.env.PRIVATE_KEY2
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     defaultNetwork: "hardhat",
@@ -16,6 +23,12 @@ module.exports = {
         localhost: {
             chainId: 31337,
         },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY1, PRIVATE_KEY2],
+            saveDeployments: true,
+            chainId: 5,
+        },
     },
     namedAccounts: {
         deployer1: {
@@ -24,11 +37,27 @@ module.exports = {
         },
         deployer2: {
             default: 1, // here this will by default take the first account as deployer
-            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+            1: 1, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
         },
         player: {
             default: 1,
         },
+    },
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY,
+        },
+        customChains: [
+            {
+                network: "goerli",
+                chainId: 5,
+                urls: {
+                    apiURL: "https://api-goerli.etherscan.io/api",
+                    browserURL: "https://goerli.etherscan.io",
+                },
+            },
+        ],
     },
 
     solidity: "0.8.17",
