7b4f64ae59c60c3bf2e634950b537525051a7269
root <root@Sarvesh-Pc.localdomain>
2023-02-19 00:44:20 +0530

    added craete Campgain + Card code restructured + pull from master + rmvl of dummy fromtend

--- client/frontend/src/redux/contants/actionType.js
+++
@@ -1,6 +0,0 @@
-export const ActionType = {
-     SET_FUNDR  : 'SET_FUNDR',
-     SET_AUTH   : 'SET_AUTH',
-     SET_FUNDRS : 'SET_FUNDRS',
-     REMOVE_SELECTED_FUNDR : 'REMOVE_SELECTED_FUNDR',
-}
--- client/frontend/src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import { fundrsReducer , fundrReducer , authReducer } from "./reducer"; 
-
-export const reducers =  combineReducers({
-     allFundrs : fundrsReducer,
-     fundr : fundrReducer,
-     auth : authReducer,
-});
