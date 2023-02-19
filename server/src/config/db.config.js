bc6591faa56a968e79d414677396a6e8148f4f48
Shreyansh <shreyanshsxn18@gmail.com>
2023-02-19 15:27:36 +0530

    removed upload/images

--- server/src/config/db.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    url: `mongodb+srv://hehe:hehe@cluster1.zb7tknd.mongodb.net/?retryWrites=true&w=majority`,
-    //url: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster1.xztjlgv.mongodb.net/?retryWrites=true&w=majority`
-};
