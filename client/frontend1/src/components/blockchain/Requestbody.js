2883025f95b8ff063a0953d910988d3ea2f394b0
root <root@Sarvesh-Pc.localdomain>
2023-02-20 15:55:38 +0530

    Handles for create Congract Hook

--- client/frontend1/src/components/blockchain/Requestbody.js
+++ client/frontend1/src/components/blockchain/Requestbody.js
@@ -35,20 +35,6 @@ export default function FundProfile({ info }) {
         },
     ])
 
-    const [requests , setRequests] = useState([{
-        "description" : "This is the sample request",
-        "amount" : 12,
-        "requestId" : "sdlfhbv989841@!3e",
-        "deadline":"25feb,2019",
-        "status":"accepted",
-    },
-    {
-        "description" : "This is the another request",
-        "amount" : 9,
-        "requestId" : "sdlfhbv989841@!3e",
-        "deadline":"3feb,2019",
-        "status":"processing",
-    }]) ; 
     
     const profile = useSelector((state) => state.fundr.fundr)
     console.log("profile from request , " , profile);
